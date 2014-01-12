fc428e78c089e5bd617f49aba2da85faa768f7a1
Shannon Byrne <s.byrne2254@gmail.com>
2014-01-12 00:04:19 -0600

    Playing around with Riot.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-alert('hi');
+var items = [
+  { name: 'TV', price: 500},
+  { name: 'Roku', price: 99},
+  { name: 'Lamp', price: 30}
+]
+
+var itemTemplate = $('#templates .item').html();
+
+for(i=0; i<= items.length; i++){
+  var newItemHtml = $.render(itemTemplate, items[i]);
+  $('#store').append(newItemHtml);
+}
