871f7658707d2d67e1131fb884e88f627014779c
Shannon Byrne <s.byrne2254@gmail.com>
2014-01-13 09:48:59 -0600

    music videos

--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@ var videos = [
   { title: 'Sadness', youtubeId: 'sIeJSSjTG3k' }
 ];
 
+var videoTemplate = $('#templates .video-list-item').html();
 var renderVideoList = function () {
-  // TODO
+  for (i=0; i <= videos.length; i++){
+    var videoHtml = $.render(videoTemplate, videos[i]);
+    $('#video-list').append(videoHtml);
+  }
 };
+
+$('button').on('click', function(e){
+  e.preventDefault();
+  var title = $('input[id=title]').val();
+  var youtubeId = $('input[id=youtubeId]').val();
+  videoHtml = $.render(videoTemplate, {title: title, youtubeId: youtubeId});
+  $('#video-list').append(videoHtml);
+  $('input').val('');
+});
+
+renderVideoList();
